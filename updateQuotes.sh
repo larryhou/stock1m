14eb4c00ebc0d978ad317af4c92399a8fd38230b
larryhou <larryhou@foxmail.com>
2015-04-20 18:03:15 +0800

    Update scripts

--- updateQuotes.sh
+++ updateQuotes.sh
@@ -5,11 +5,19 @@ if [ ! -d "${dir}" ]
 then
 	mkdir ${dir}
 fi
-cat HKG.t SHE.t SHA.t | while read ticket
-do
-	echo "fetch ${ticket} ..."
-	output=${dir}/${ticket}
-	touch ${output}
-	googleQuote -t ${ticket} -d 100 > ${output}
-	sort -k 1 -k 2 -r -o ${output} ${output}
-done
+
+fetchQuotes()
+{
+	cat ${1}.t | while read ticket
+	do
+		echo "fetch ${ticket} ..."
+		output=${dir}/${ticket}
+		touch ${output}
+		googleQuote -t ${ticket} -d 100 -x ${1}  > ${output}
+		sort -k 1 -k 2 -r -o ${output} ${output}
+	done
+}
+
+fetchQuotes SHE
+fetchQuotes SHA
+fetchQuotes HKG
